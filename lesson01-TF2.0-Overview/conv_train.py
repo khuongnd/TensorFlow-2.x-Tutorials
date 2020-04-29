62ce54eef75ff25a5a072e314dd440080b565eea
khuongnd <Huong@209>
2020-04-29 10:14:14 +0700

    Revise code

--- lesson01-TF2.0-Overview/conv_train.py
+++ lesson01-TF2.0-Overview/conv_train.py
@@ -1,30 +1,35 @@
 import os
 import time
 import numpy as np
+
 import tensorflow as tf
 from tensorflow.python.ops import summary_ops_v2
 from tensorflow import keras
 from tensorflow.keras import datasets, layers, models, optimizers, metrics
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # or any {'0', '1', '2'}
+
+def create_model():
+    model = tf.keras.Sequential([
+        layers.Reshape(
+            target_shape=[28, 28, 1],
+            input_shape=(28, 28,)),
+        layers.Conv2D(2, 5, padding='same', activation=tf.nn.relu),
+        layers.MaxPooling2D((2, 2), (2, 2), padding='same'),
+        layers.Conv2D(4, 5, padding='same', activation=tf.nn.relu),
+        layers.MaxPooling2D((2, 2), (2, 2), padding='same'),
+        layers.Flatten(),
+        layers.Dense(32, activation=tf.nn.relu),
+        layers.Dropout(rate=0.4),
+        layers.Dense(10)]
+    )
+    return model
 
 
-model = tf.keras.Sequential([
-    layers.Reshape(
-        target_shape=[28, 28, 1],
-        input_shape=(28, 28,)),
-    layers.Conv2D(2, 5, padding='same', activation=tf.nn.relu),
-    layers.MaxPooling2D((2, 2), (2, 2), padding='same'),
-    layers.Conv2D(4, 5, padding='same', activation=tf.nn.relu),
-    layers.MaxPooling2D((2, 2), (2, 2), padding='same'),
-    layers.Flatten(),
-    layers.Dense(32, activation=tf.nn.relu),
-    layers.Dropout(rate=0.4),
-    layers.Dense(10)])
-
-compute_loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-compute_accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
-optimizer = optimizers.SGD(learning_rate=0.01, momentum=0.5)
+def complie_model():
+    compute_loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    compute_accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
+    optimizer = optimizers.SGD(learning_rate=0.01, momentum=0.5)
+    return compute_loss, compute_accuracy, optimizer
 
 
 def mnist_datasets():
@@ -37,23 +42,23 @@ def mnist_datasets():
     return train_dataset, test_dataset
 
 
-train_ds, test_ds = mnist_datasets()
-train_ds = train_ds.shuffle(60000).batch(100)
-test_ds = test_ds.batch(100)
+def split_data():
+    train_ds, test_ds = mnist_datasets()
+    train_ds = train_ds.shuffle(60000).batch(100)
+    test_ds = test_ds.batch(100)
+    return train_ds, test_ds
 
 
 def train_step(model, optimizer, images, labels):
-
     # Record the operations used to compute the loss, so that the gradient
     # of the loss with respect to the variables can be computed.
+    global compute_loss, compute_accuracy
     with tf.GradientTape() as tape:
         logits = model(images, training=True)
         loss = compute_loss(labels, logits)
         compute_accuracy(labels, logits)
-
     grads = tape.gradient(loss, model.trainable_variables)
     optimizer.apply_gradients(zip(grads, model.trainable_variables))
-
     return loss
 
 
@@ -64,7 +69,7 @@ def train(model, optimizer, dataset, log_freq=50):
     # Metrics are stateful. They accumulate values and return a cumulative
     # result when you call .result(). Clear accumulated values with .reset_states()
     avg_loss = metrics.Mean('loss', dtype=tf.float32)
-
+    global compute_accuracy
     # Datasets can be iterated over like any other Python iterable.
     for images, labels in dataset:
         loss = train_step(model, optimizer, images, labels)
@@ -85,22 +90,16 @@ def test(model, dataset, step_num):
     Perform an evaluation of `model` on the examples from `dataset`.
     """
     avg_loss = metrics.Mean('loss', dtype=tf.float32)
-
+    global compute_loss
     for (images, labels) in dataset:
         logits = model(images, training=False)
         avg_loss(compute_loss(labels, logits))
         compute_accuracy(labels, logits)
-
     print('Model test set loss: {:0.4f} accuracy: {:0.2f}%'.format(
         avg_loss.result(), compute_accuracy.result() * 100))
-
     print('loss:', avg_loss.result(), 'acc:', compute_accuracy.result())
     # summary_ops_v2.scalar('loss', avg_loss.result(), step=step_num)
     # summary_ops_v2.scalar('accuracy', compute_accuracy.result(), step=step_num)
-
-
-# Where to save checkpoints, tensorboard summaries, etc.
-MODEL_DIR = '/tmp/tensorflow/mnist'
 
 
 def apply_clean():
@@ -109,30 +108,36 @@ def apply_clean():
         tf.io.gfile.rmtree(MODEL_DIR)
 
 
-apply_clean()
+if __name__ == "__main__":
+    # Where to save checkpoints, tensorboard summaries, etc.
+    MODEL_DIR = '/tmp/tensorflow/mnist'
+    apply_clean()
 
-checkpoint_dir = os.path.join(MODEL_DIR, 'checkpoints')
-checkpoint_prefix = os.path.join(checkpoint_dir, 'ckpt')
+    model = create_model()
+    checkpoint_dir = os.path.join(MODEL_DIR, 'checkpoints')
+    checkpoint_prefix = os.path.join(checkpoint_dir, 'ckpt')
+    compute_loss, compute_accuracy, optimizer = complie_model()
 
-checkpoint = tf.train.Checkpoint(model=model, optimizer=optimizer)
+    checkpoint = tf.train.Checkpoint(model=model, optimizer=optimizer)
+    train_ds, test_ds = split_data()
 
-# Restore variables on creation if a checkpoint exists.
-checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
+    # Restore variables on creation if a checkpoint exists.
+    checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
 
-NUM_TRAIN_EPOCHS = 5
+    NUM_TRAIN_EPOCHS = 5
 
-for i in range(NUM_TRAIN_EPOCHS):
-    start = time.time()
-    #   with train_summary_writer.as_default():
-    train(model, optimizer, train_ds, log_freq=500)
-    end = time.time()
-    print('Train time for epoch #{} ({} total steps): {}'.format(
-        i + 1, int(optimizer.iterations), end - start))
-    #   with test_summary_writer.as_default():
-    #     test(model, test_ds, optimizer.iterations)
-    checkpoint.save(checkpoint_prefix)
-    print('saved checkpoint.')
+    for i in range(NUM_TRAIN_EPOCHS):
+        start = time.time()
+        #   with train_summary_writer.as_default():
+        train(model, optimizer, train_ds, log_freq=500)
+        end = time.time()
+        print('Train time for epoch #{} ({} total steps): {}'.format(
+            i + 1, int(optimizer.iterations), end - start))
+        #   with test_summary_writer.as_default():
+        #     test(model, test_ds, optimizer.iterations)
+        checkpoint.save(checkpoint_prefix)
+        print('saved checkpoint.')
 
-export_path = os.path.join(MODEL_DIR, 'export')
-tf.saved_model.save(model, export_path)
-print('saved SavedModel for exporting.')
+    export_path = os.path.join(MODEL_DIR, 'export')
+    tf.saved_model.save(model, export_path)
+    print('saved SavedModel for exporting.')
