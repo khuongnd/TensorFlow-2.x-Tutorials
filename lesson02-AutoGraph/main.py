d07bfa2517a43d7ef2f56426a4d4ec8df295f50d
Khuong Nguyen <khuongnd@gmail.com>
2021-03-27 23:59:33 +0700

    Normalize code

--- lesson02-AutoGraph/main.py
+++ lesson02-AutoGraph/main.py
@@ -1,39 +1,24 @@
-import  tensorflow as tf
-
+import tensorflow as tf
 import timeit
-
-
-
-
 
 cell = tf.keras.layers.LSTMCell(10)
 
 
 @tf.function
 def fn(input, state):
-    """
-    use static graph to compute LSTM
-    :param input:
-    :param state:
-    :return:
-    """
-
+    '''
+    Calculate output of cell (input, state)
+    '''
     return cell(input, state)
 
 
-
 input = tf.zeros([10, 10])
-state = [tf.zeros([10, 10])] * 2
+state = [tf.ones([10, 10])] * 2
 
 # warmup
-cell(input, state)
-fn(input, state)
-
+cell(input, state), fn(input, state)
 
 dynamic_graph_time = timeit.timeit(lambda: cell(input, state), number=100)
 static_graph_time = timeit.timeit(lambda: fn(input, state), number=100)
 print('dynamic_graph_time:', dynamic_graph_time)
 print('static_graph_time:', static_graph_time)
-
-
-
