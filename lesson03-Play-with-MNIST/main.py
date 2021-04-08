f6eef49c4e51b7203dc61f7c0bebdaa6da83f786
Khuong Nguyen <khuongnd@gmail.com>
2021-04-08 16:01:45 +0700

    Normalize code

--- lesson03-Play-with-MNIST/main.py
+++ lesson03-Play-with-MNIST/main.py
@@ -1,18 +1,19 @@
 import tensorflow as tf
-from tensorflow.keras import datasets, layers, optimizers, Sequential, metrics
+from tensorflow.keras import layers, optimizers, Sequential, metrics
 from pagi.utils.data import tf_get_mnist_dataset
 
-ds = tf_get_mnist_dataset(bath_size=128)
+bath_size = 128
+ds = tf_get_mnist_dataset(bath_size=bath_size)
 
 network = Sequential([layers.Dense(256, activation='relu'),
                       layers.Dense(256, activation='relu'),
                       layers.Dense(256, activation='relu'),
-                      layers.Dense(10)])
-
+                      layers.Dense(10)]
+                     )
 network.build(input_shape=(None, 28 * 28))
 network.summary()
 
-optimizer = optimizers.SGD(lr=0.01)
+optimizer = optimizers.Adam(learning_rate=0.01)
 acc_meter = metrics.Accuracy()
 
 for iter in range(20):
@@ -22,11 +23,10 @@ for iter in range(20):
             out = network(x)
             y_onehot = tf.one_hot(y, depth=10)
             loss = tf.square(out - y_onehot)
-            loss = tf.reduce_sum(loss) / 32
-
+            loss = tf.reduce_sum(loss) / bath_size
         acc_meter.update_state(tf.argmax(out, axis=1), y)
         grads = tape.gradient(loss, network.trainable_variables)
         optimizer.apply_gradients(zip(grads, network.trainable_variables))
-
+    #
     print(iter, 'loss:', float(loss), 'acc:', acc_meter.result().numpy())
     acc_meter.reset_states()
