d0542c65a3f352cd1b869d052da334ef578d21ba
Khuong Nguyen <khuongnd@gmail.com>
2021-06-01 15:38:23 +0700

    Normalize code

--- lesson14-FashionMNIST/mnist_Seqential_gradient.py
+++ lesson14-FashionMNIST/mnist_Seqential_gradient.py
@@ -1,65 +1,59 @@
-import  os
-import  tensorflow as tf
-from    tensorflow import keras
-from    tensorflow.keras import layers, optimizers, datasets
+import os
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers, optimizers, datasets
+
 
 def prepare_mnist_features_and_labels(x, y):
-  x = tf.cast(x, tf.float32) / 255.0
-  y = tf.cast(y, tf.int64)
-  return x, y
+    x = tf.cast(x, tf.float32) / 255.0
+    y = tf.cast(y, tf.int64)
+    return x, y
+
 
 def mnist_dataset():
-  (x, y), _ = datasets.fashion_mnist.load_data()
-  ds = tf.data.Dataset.from_tensor_slices((x, y))
-  ds = ds.map(prepare_mnist_features_and_labels)
-  ds = ds.take(20000).shuffle(20000).batch(100)
-  return ds
-
-
-
-
+    (x, y), _ = datasets.fashion_mnist.load_data()
+    ds = tf.data.Dataset.from_tensor_slices((x, y))
+    ds = ds.map(prepare_mnist_features_and_labels)
+    ds = ds.take(20000).shuffle(20000).batch(100)
+    return ds
 
 
 def compute_loss(logits, labels):
-  return tf.reduce_mean(
-      tf.nn.sparse_softmax_cross_entropy_with_logits(
-          logits=logits, labels=labels))
+    return tf.reduce_mean(
+        tf.nn.sparse_softmax_cross_entropy_with_logits(
+            logits=logits, labels=labels))
 
 
 def compute_accuracy(logits, labels):
-  predictions = tf.argmax(logits, axis=1)
-  return tf.reduce_mean(tf.cast(tf.equal(predictions, labels), tf.float32))
+    predictions = tf.argmax(logits, axis=1)
+    return tf.reduce_mean(tf.cast(tf.equal(predictions, labels), tf.float32))
 
 
 def train_one_step(model, optimizer, x, y):
+    with tf.GradientTape() as tape:
+        logits = model(x)
+        loss = compute_loss(logits, y)
 
-  with tf.GradientTape() as tape:
+    # compute gradient
+    grads = tape.gradient(loss, model.trainable_variables)
+    # update to weights
+    optimizer.apply_gradients(zip(grads, model.trainable_variables))
 
-    logits = model(x)
-    loss = compute_loss(logits, y)
+    accuracy = compute_accuracy(logits, y)
 
-  # compute gradient
-  grads = tape.gradient(loss, model.trainable_variables)
-  # update to weights
-  optimizer.apply_gradients(zip(grads, model.trainable_variables))
-
-  accuracy = compute_accuracy(logits, y)
-
-  # loss and accuracy is scalar tensor
-  return loss, accuracy
+    # loss and accuracy is scalar tensor
+    return loss, accuracy
 
 
 def train(epoch, model, optimizer):
-
-  train_ds = mnist_dataset()
-  loss = 0.0
-  accuracy = 0.0
-  for step, (x, y) in enumerate(train_ds):
-    loss, accuracy = train_one_step(model, optimizer, x, y)
-    if step%500==0:
-      print('epoch', epoch, ': loss', loss.numpy(), '; accuracy', accuracy.numpy())
-  return loss, accuracy
-
+    train_ds = mnist_dataset()
+    loss = 0.0
+    accuracy = 0.0
+    for step, (x, y) in enumerate(train_ds):
+        loss, accuracy = train_one_step(model, optimizer, x, y)
+        if step % 500 == 0:
+            print('epoch', epoch, ': loss', loss.numpy(), '; accuracy', accuracy.numpy())
+    return loss, accuracy
 
 
 def main():
@@ -72,7 +66,6 @@ def main():
         layers.Dense(200, activation='relu'),
         layers.Dense(200, activation='relu'),
         layers.Dense(10)])
-
 
     optimizer = optimizers.Adam()
 
